67f3a3d99d3e7f46af9201709d1c18e6deb6cdac
hoa luu <luuduchoa2504@gmail.com>
2022-10-27 17:51:59 +0700

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.scss']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [
+    new Recipe('A', 'Simple Test', 'https://img.buzzfeed.com/video-api-prod/assets/eb44570519264864814264f7f0a5e47a/BFV13909_BakedRatatouille-ThumbTextless1080.jpg?resize=1200:*')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
